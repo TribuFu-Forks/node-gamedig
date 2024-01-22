b56eff7783af32caccaa28a96775b9d920ece9ec
Guilherme Werner <guilherme.werner@tribufu.com>
2024-01-22 11:49:28 -0300

    Move attributes to game protocol

--- protocols/asa.js
+++ protocols/asa.js
@@ -9,4 +9,11 @@ export default class asa extends Epic {
     this.clientSecret = 'PP5UGxysEieNfSrEicaD1N2Bb3TdXuD7xHYcsdUHZ7s'
     this.deploymentId = 'ad9a8feffb3b4b2ca315546f038c3ae2'
   }
+
+  async run (state) {
+    await super.run(state)
+    state.name = state.raw.attributes.CUSTOMSERVERNAME_s
+    state.map = state.raw.attributes.MAPNAME_s
+    state.password = state.raw.attributes.SERVERPASSWORD_b
+  }
 }
